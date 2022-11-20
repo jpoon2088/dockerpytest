4bc51fe768774cb556c8221216b64bc77865640a
root <root@localhost.localdomain>
2022-11-19 19:57:17 -0800

    points to port 80

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import docker
 ##client = docker.APIClient(base_url='unix://var/run/docker.sock')
-client = docker.APIClient(base_url='tcp://127.0.0.1:1234', timeout=5)
+client = docker.APIClient(base_url='tcp://127.0.0.1:80', timeout=5)
 pullresult = client.pull("alpine")
 listresult = client.containers(all='true')
 print(listresult)
